ec9d51ad6299b14947d145ec0636ece3e421ec91
Abdulhakeem Ahmed <keemahmed99@gmail.com>
2025-12-03 10:26:15 +0100

    done

---
+++ quiz.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+const feedback = document.getElementById("feedback");
+const submit_answer =  document.getElementById("submit-answer");
+function checkanswer() {
+   
+    let correctanswer = "4";
+    let ans = document.querySelector('input[name="quiz"]:checked');
+    let useranswer = ans.value;
+
+    if (correctanswer===useranswer){
+        feedback.textContent = "Correct! Well done." ;
+    }
+    else{
+        feedback.textContent = "That's incorrect. Try again!" ;
+    }
+
+};
+
+submit_answer.addEventListener('submit',checkanswer);
+});
+
+
